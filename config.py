9df1556fac4ab13a66787e5d1a821d4f3697e6ac
akasaka-kun <jules.03@orange.fr>
2023-05-18 13:49:24 +0200

    added projectile directions.

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import pygame
 
 # user configs
-size = (800, 800)
+size = (1000, 1000)
 keyboard_controls = {
     pygame.K_w: "up",
     pygame.K_a: "left",
     pygame.K_s: "down",
     pygame.K_d: "right",
     pygame.K_LSHIFT: "focus",
-    pygame.K_UP: "shoot"
+    pygame.K_UP: "shootUp",
+    pygame.K_DOWN: "shootDown",
+    pygame.K_LEFT: "shootLeft",
+    pygame.K_RIGHT: "shootRight",
 }
 
 # game configs
--- player.py
+++ player.py
@@ -15,14 +15,17 @@ V = 200
 
 
 class test_bullet(projectile.projectile):
-    surface = [(b := pygame.Surface((10, 10))).fill((255, 0, 0)), b][1]  # this is cursed and i love it
+    surface = [(b := pygame.Surface((10, 10))).fill((255, 0, 0)), b][1]  # this is cursed and I love it
+    SPEED = 300
 
     @staticmethod
     def update_pos(pos, time, **kwargs) -> dict:
-        return {"pos": kwargs["spawn_pos"] + (numpy.array([math.cos(kwargs["dir"]), math.sin(kwargs["dir"])], dtype=float) * (time*200))}
+        return {"pos": kwargs["spawn_pos"] + (numpy.array([math.cos(kwargs["dir"]), math.sin(kwargs["dir"])], dtype=float) * (time*test_bullet.SPEED))}
 
 
 class Player(Renderable):
+
+    BULLET_DELAY = .150
 
     def __init__(self, controller):
         GLOBAL.TO_RENDER.append(self)
@@ -35,6 +38,7 @@ class Player(Renderable):
         self.y = 800
 
         self.danmaku = projectile.Danmaku(test_bullet, self.faction)
+        self.bulletCD = 0
 
         S = pygame.Surface((20, 20)).convert_alpha()
         S.fill((255, 0, 0, 255))
@@ -46,8 +50,11 @@ class Player(Renderable):
 
     def update(self, dt):
         self.v.fill(0)
+        self.bulletCD = max(0, self.bulletCD-dt)
         for a in self.controller.actions:  # todo state detection for animation system
             match a:
+
+                # move
                 case "up":
                     self.v[1] = -V
                 case "left":
@@ -58,6 +65,23 @@ class Player(Renderable):
                     self.v[0] = V
                 case "focus":
                     self.v = self.v / 1.5
-                case "shoot":
-                    self.danmaku.add_bullet(self.pos, dir=1.5*math.pi, spawn_pos=self.pos)
+
+                # shoot
+                case "shootUp":
+                    if self.bulletCD == 0:
+                        self.bulletCD = Player.BULLET_DELAY
+                        self.danmaku.add_bullet(self.pos, dir=1.5*math.pi, spawn_pos=self.pos)
+                case "shootDown":
+                    if self.bulletCD == 0:
+                        self.bulletCD = Player.BULLET_DELAY
+                        self.danmaku.add_bullet(self.pos, dir=.5*math.pi, spawn_pos=self.pos)
+                case "shootLeft":
+                    if self.bulletCD == 0:
+                        self.bulletCD = Player.BULLET_DELAY
+                        self.danmaku.add_bullet(self.pos, dir=math.pi, spawn_pos=self.pos)
+                case "shootRight":
+                    if self.bulletCD == 0:
+                        self.bulletCD = Player.BULLET_DELAY
+                        self.danmaku.add_bullet(self.pos, dir=0, spawn_pos=self.pos)
+
         self.x, self.y = [bind(i, bound_rect[0][n], bound_rect[1][n]) for n, i in enumerate((self.x, self.y) + self.v * dt)]
