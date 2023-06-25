2ea6b02ee7eaccd4cf0af4c0bc8c6e8ee843d897
akasaka-kun <jules.03@orange.fr>
2023-06-25 22:23:40 +0200

    updated default level to utilize the new simultaneous events

--- levels.py
+++ levels.py
@@ -33,16 +33,16 @@ class Level:
 
 # todo change levels system to allow multiple simultaneous events
 Default = Level({
-    1: LEvent(TestEnemy.spawn, [(200, 100)]),
-    1.3: LEvent(TestEnemy.spawn, [(400, 100)]),
-    1.6: LEvent(TestEnemy.spawn, [(600, 100)]),
-    1.9: LEvent(TestEnemy.spawn, [(800, 100)]),
-    3: LEvent(TestEnemy.spawn, [(200, 100)]),
-    3.3: LEvent(TestEnemy.spawn, [(400, 100)]),
-    3.6: LEvent(TestEnemy.spawn, [(600, 100)]),
-    3.9: LEvent(TestEnemy.spawn, [(800, 100)]),
-    5: LEvent(TestEnemy.spawn, [(200, 100)]),
-    5.3: LEvent(TestEnemy.spawn, [(400, 100)]),
-    5.6: LEvent(TestEnemy.spawn, [(600, 100)]),
-    5.9: LEvent(TestEnemy.spawn, [(800, 100)]),
+    1: [LEvent(TestEnemy.spawn, [(200, 100)]), LEvent(TestEnemy.spawn, [(200, 100), "right"])],
+    1.3: [LEvent(TestEnemy.spawn, [(400, 100)]), LEvent(TestEnemy.spawn, [(400, 100), "right"])],
+    1.6: [LEvent(TestEnemy.spawn, [(600, 100)]), LEvent(TestEnemy.spawn, [(600, 100), "right"])],
+    1.9: [LEvent(TestEnemy.spawn, [(800, 100)]), LEvent(TestEnemy.spawn, [(800, 100), "right"])],
+    3: [LEvent(TestEnemy.spawn, [(200, 100)]), LEvent(TestEnemy.spawn, [(200, 100), "right"])],
+    3.3: [LEvent(TestEnemy.spawn, [(400, 100)]), LEvent(TestEnemy.spawn, [(400, 100), "right"])],
+    3.6: [LEvent(TestEnemy.spawn, [(600, 100)]), LEvent(TestEnemy.spawn, [(600, 100), "right"])],
+    3.9: [LEvent(TestEnemy.spawn, [(800, 100)]), LEvent(TestEnemy.spawn, [(800, 100), "right"])],
+    5: [LEvent(TestEnemy.spawn, [(200, 100)]), LEvent(TestEnemy.spawn, [(200, 100), "right"])],
+    5.3: [LEvent(TestEnemy.spawn, [(400, 100)]), LEvent(TestEnemy.spawn, [(400, 100), "right"])],
+    5.6: [LEvent(TestEnemy.spawn, [(600, 100)]), LEvent(TestEnemy.spawn, [(600, 100), "right"])],
+    5.9: [LEvent(TestEnemy.spawn, [(800, 100)]), LEvent(TestEnemy.spawn, [(800, 100), "right"])],
 })
